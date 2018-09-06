dbdccfd59b15a73be20873b7271f1d9ae84a660a
Chris Lamb <chrislambistan@gmail.com>
2018-09-05 21:27:59 -0600

    work

--- comms/static/main.js
+++ comms/static/main.js
@@ -1,9 +1,39 @@
 const commands = {
 
-  commands: new Array(),
+	commands: new Array(),
 
-  execute: function(fn) {
-    this.commands.push(fn);
-  }
+	execute: function(fn) {
+    	this.commands.push(fn);
+  	},
+
+  	get_commands: function() {
+  		try {
+  			this.lock = true;
+
+  			poll(server_host, function(resp) {
+  				if (resp.body != null && resp.body.length > 0) {
+  					this.execute_commands();
+  				}
+  			});
+  		} catch(e) {
+  			this.lock = false;
+  			return;
+  		}
+  		this.lock = false;
+  	}
+
+  	execute_commands: function() {
+  		if (commands.length <= 0) return;
+  		this.lock = true;
+  		while(commands.length > 0) {
+  			let command = this.commands.pop()
+  			try {
+  				command();
+  			} catch(e) {
+  				console.error(.message)
+  			}
+  		}
+  		this.lock = false;
+  	}
 
 };
--- scratch/main.py
+++ scratch/main.py
@@ -1,12 +1,18 @@
 #
 # http://localhost:5000/?user=<script>alert('foo');</script>
 #
+import os
+
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def exploitable():
